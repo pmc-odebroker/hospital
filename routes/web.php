204099b269865220ed0af11d6c1a544cab44a7e0
pmc-odebroker <patrickcharlestz@gmail.com>
2024-01-17 07:43:28 +0300

    create doctor page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,7 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/home', [HomeController::class, 'redirect']);
+Route::get('/add-doctor', [AdminController::class, 'add_doctor']);
 
 Route::middleware([
     'auth:sanctum',
